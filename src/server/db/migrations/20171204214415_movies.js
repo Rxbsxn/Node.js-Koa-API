e957b9954a0b59381de0f88a7d256e93d74b0368
Robert Harężlak <harezlak.robert7@gmail.com>
2017-12-04 21:47:11 +0100

    database configuration

--- src/server/db/migrations/20171204214415_movies.js
+++ src/server/db/migrations/20171204214415_movies.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('movies', (tables) => {
+    table.increments();
+    table.string('name').notNullable().unique();
+    table.string('genre').notNullable();
+    table.integer('rating').notNullable();
+    table.boolean('explicit').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTables('movies');
 };
