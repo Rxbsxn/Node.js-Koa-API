b33f24ac9fbe877148b574ae81a325ea0f9111d3
Robert Harężlak <harezlak.robert7@gmail.com>
2017-12-04 21:52:40 +0100

    seeds

--- src/server/db/seeds/movies_seed.js
+++ src/server/db/seeds/movies_seed.js
@@ -1,13 +1,27 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+exports.seed = (knex, Promise) => {
+  return knex('movies').del()
+  .then(() => {
+    return knex('movies').insert({
+      name: 'The Land Before Time',
+      genre: 'Fantasy',
+      rating: 7,
+      explicit: false
     });
+  })
+  .then(() => {
+    return knex('movies').insert({
+      name: 'Jurassic Park',
+      genre: 'Science Fiction',
+      rating: 9,
+      explicit: true
+    });
+  })
+  .then(() => {
+    return knex('movies').insert({
+      name: 'Ice Age: Dawn of the Dinosaurs',
+      genre: 'Action/Romance',
+      rating: 5,
+      explicit: false
+    });
+  });
 };
