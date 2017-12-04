7aed1add19f60553c97552ace37afe26bea9ccee
Robert Harężlak <harezlak.robert7@gmail.com>
2017-12-04 21:38:29 +0100

    make test pass

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,17 @@
-console.log('It works!');
+const Koa = require('koa');
+
+const app = new Koa();
+const PORT = 3000;
+
+app.use(async (ctx) => {
+  ctx.body = {
+    status: 'success',
+    message: 'hello, world!'
+  };
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
+
+module.exports = server;
